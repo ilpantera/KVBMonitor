4f4d0eca63bb85faf1a09f3988eb2315a46077a7
ilpantera <62816136+ilpantera@users.noreply.github.com>
2020-03-28 23:55:41 +0100

    Update kvbmonitor.py
    Saubere Version der kvb-sample ohne "double buffering"

--- kvbmonitor.py
+++ kvbmonitor.py
@@ -1,70 +1,91 @@
-import schedule
+#!/usr/bin/env python
+from samplebase import SampleBase
+from rgbmatrix import graphics
 import time
+import datetime
+from itertools import islice
 from bs4 import BeautifulSoup
 import requests
 import threading
-from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
-
-STATION_ID = '632'
-MATRIX_COLS = 32
-MATRIX_ROWS = 64
-
-HEADERS = {
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36"
-}
-
-options = RGBMatrixOptions()
-options.rows = MATRIX_ROWS
-options.cols = MATRIX_COLS
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'regular'
-
-matrix = RGBMatrix(options = options)
-
-canvas = matrix.CreateFrameCanvas()
-font = graphics.Font()
-font.LoadFont("resources/fonts/4x6.bdf")
-textColor = graphics.Color(255, 255, 0)
-pos = canvas.width
-
-def get_departures():
-    url = "https://kvb.koeln/qr/%d/" % int(STATION_ID)
-    r = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    tables = soup.find_all("table", class_="display")
-    departures = []
-    for row in tables[0].find_all("tr"):
-        tds = row.find_all("td")
-        (line_id, direction, time) = (tds[0].text, tds[1].text, tds[2].text)
-        line_id = line_id.replace(u"\xa0", "")
-        direction = direction.replace(u"\xa0", "")
-        time = time.replace(u"\xa0", " ").strip().lower()
-        try:
-            line_id = int(line_id)
-        except:
-            pass
-        if(direction == "Bocklemünd" or direction == "Rochusplatz"):
-            departures.append({
-                "line_id": line_id,
-                "direction": direction,
-                "wait_time": time
-            })
-    return departures
-
-def print_departures_to_matrix():
-    canvas.Clear()
-    lines = []
-    for d in get_departures():
-        line = "{0}    {1}    {2}".format(str(d['line_id']), d['direction'], d['wait_time'])
-        print(line)
-        graphics.DrawText(canvas, font, pos, 10, textColor, line)
 
 
-print_departures_to_matrix()
-schedule.every(10).seconds.do(print_departures_to_matrix)
+class KVBMonitorAusgabe(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(KVBMonitorAusgabe, self).__init__(*args, **kwargs)
 
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
-    
+    def run(self):
+        canvas = self.matrix
+        font = graphics.Font()
+        font.LoadFont("resources/fonts/5x7.bdf")
+        # Definition der Schriftfarbe
+        textColor = graphics.Color(255, 255, 255)
+        HEADERS = {
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36",
+            "Connection": "close"
+        
+        }
+        
+        url = "https://kvb.koeln/qr/632/"
+
+        while True:
+            r = requests.get(url, headers=HEADERS)
+            soup = BeautifulSoup(r.text, features="html.parser")
+            tables = soup.find_all("table", class_="display")
+            departures = []
+            for row in tables[0].find_all("tr"):
+                tds = row.find_all("td")
+                (line_id, direction, wait_time) = (tds[0].text, tds[1].text, tds[2].text)
+                line_id = line_id.replace(u"\xa0", "")
+                direction = direction.replace(u"\xa0", "")
+                if(wait_time == "Sofort"): wait_time = "0 Min"
+                wait_time = wait_time.replace(u" Min", "m").strip()
+                if(len(wait_time) == 2): wait_time = " {0}".format(wait_time)
+
+                if(wait_time == " 4m"): wait_time = "3+1m"
+                if(wait_time == " 9m"): wait_time = "09m"
+
+                try:
+                    line_id = int(line_id)
+                except:
+                    pass
+                if(direction == "Bocklemünd" or direction == "Rochusplatz"):
+                    departures.append({
+                        "line_id": line_id,
+                        "direction": direction,
+                        "wait_time": wait_time
+                    })
+            soup.decompose()
+            canvas.Clear()
+
+            # Display the current date and time on top
+            now = datetime.datetime.now()
+            curr_date = now.strftime("%d.%m.")
+            curr_time = now.strftime("%H:%M")
+            graphics.DrawText(canvas, font, 2, 8, textColor, curr_date)
+            graphics.DrawText(canvas, font, 37, 8, textColor, curr_time)
+            
+            # Set y-position of first connection to 16px
+            ymargin = 19
+
+            #Kommandozeilenausgabe aktivieren
+            #print("{0}        {1}".format(curr_date, curr_time))
+
+            departures = islice(departures, 2)
+
+            for depart in departures:
+                if(depart['direction'] == "Bocklemünd"): depart['direction'] = "Bockl."
+                if(depart['direction'] == "Rochusplatz"): depart['direction'] = "Rochu."
+                connection = "{0} {1} {2}".format(str(depart['line_id']), depart['direction'], depart['wait_time'])    
+                
+                #Kommandozeilenausgabe aktivieren
+                #print(connection)
+                graphics.DrawText(canvas, font, 2, ymargin, textColor, connection)
+                ymargin = ymargin + 10
+
+            time.sleep(5) 
+
+# Main function
+if __name__ == "__main__":
+    KVBMonitor_Ausgabe = KVBMonitorAusgabe()
+    if (not KVBMonitor_Ausgabe.process()):
+        KVBMonitor_Ausgabe.print_help()
